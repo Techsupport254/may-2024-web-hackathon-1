954c1d3ebb1f23d0f5ca4f43c2be31d5d7a7fdce
Victor Kipkorir <victorquaint@gmail.com>
2023-07-08 14:22:43 +0300

    Deploy backend

--- Backend/Server.jsx
+++ Backend/Server.jsx
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
 	cors({
-		origin: ["http://localhost:5173", "http://localhost:3000"],
+		origin: ["http://localhost:5173", "https://agrisolve-9vas.vercel.app/"],
 		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 		credentials: true,
 	})
